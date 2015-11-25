114ffe8c2458667818e8e1deacbb70084f0fdf82
Anthony Rosario <ajrosario08@gmail.com>
2015-11-25 13:30:54 -0800

    added view object with BouncerBehavior to view

--- Bouncer/ViewController.swift
+++ Bouncer/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let bouncer = BouncerBehavior()
+    lazy var animator: UIDynamicAnimator = {UIDynamicAnimator(referenceView: self.view)}()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        animator.addBehavior(bouncer)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    struct Constance {
+        static let BlockSize = CGSize(width: 40, height: 40)
     }
-
-
+    
+    var redBlock: UIView?
+    
+    override func viewDidAppear(animated: Bool) {
+        if redBlock == nil {
+            redBlock = addBlock()
+            redBlock?.backgroundColor = UIColor.redColor()
+            bouncer.addBlock(redBlock!)
+        }
+    }
+    
+    func addBlock() -> UIView {
+        let block = UIView(frame: CGRect(origin: CGPointZero, size: Constance.BlockSize))
+        block.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
+        view.addSubview(block)
+        return block
+    }
+    
+    
 }
 
